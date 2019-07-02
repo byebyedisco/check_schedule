895f9a11b3e54acd70ff9177b928d47f5c826ea5
xiezi <haeckel.xhh@foxmail.com>
2019-07-02 23:25:02 +0800

    2.0.2

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -4,20 +4,23 @@ const app = getApp()
 
 Page({
   data: {
-    schedule : ""
+    schedule : "",
+    role_array : ['A','B','C','D','E'],
+    role_index : 0,
   },
   onLoad: function() {},
   handleSelecteDate(e) {
     // wx.showToast({ title: `${e.detail.date}`, icon: false })
     // console.log(e.detail.date);
     var targetDate = new Date(e.detail.date);
-    var role = 'A';//ABCDE
+    var role = this.data.role_array[this.data.role_index];//ABCDE
+    // console.log(role);
     // Set the unit values in milliseconds.
     var msecPerMinute = 1000 * 60;
     var msecPerHour = msecPerMinute * 60;
     var msecPerDay = msecPerHour * 24;
 
-    var initDate = new Date("2019-06-03");
+    var initDate = new Date("2019-06-24");
 
     //20190630 新的排班表
 
@@ -29,13 +32,14 @@ Page({
 
     var day_schedule = [];
     var index = (days % 28) + 1;//第几天 从第一天计数
+    // console.log(index);
     //周六日白班
     var sat_day = ['D', 'C', 'B', 'E'];
     var sun_day = ['E', 'D', 'C', 'B'];
     //平时中班
     var weekday_mid = ['B', 'C', 'D', 'E'];
     //平时夜班
-    var weekday_night = ['D', 'B', 'C', 'D'];
+    var weekday_night = ['E', 'B', 'C', 'D'];
 
     //周末白班计算
     if ((index + 1) % 7 == 0){
@@ -48,17 +52,27 @@ Page({
       day_schedule[0] = 'A';
     }
     //中班夜班计算
-    day_schedule[1] = weekday_mid[(index%4) - 1];
-    day_schedule[2] = weekday_night[(index%4) - 1];
+    day_schedule[1] = weekday_mid[(index-1) % 4];
+    day_schedule[2] = weekday_night[(index-1) % 4];
 
+    console.log(day_schedule);
+
+    //检查角色当天排班情况
     var sch_index = day_schedule.indexOf(role);
     if (sch_index < 0){
       sch_index = 3;
     }
 
+    //页面显示
     this.data.schedule = schedule[sch_index];
     this.setData({
         schedule : this.data.schedule
     });
+  },
+  bindPickerChange: function (e) {
+    console.log('角色选择改为', e.detail.value)
+    this.setData({
+      role_index: e.detail.value
+    })
   },
 })
